c67b9f911b68dd762dd14e40a29f44de6cee2c09
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-08 19:18:35 +0100

    feat: transform data with current pomodoro to struct

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -47,7 +48,58 @@ func (m model) getPomodoroData() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(props)
+	pomodoro, err := TransformPomodoro(props)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(pomodoro)
+}
+
+type Pomodoro struct {
+	IsPaused bool
+	Elapsed float64
+	State string
+	StateDuration int
+}
+
+func TransformPomodoro(props map[string]dbus.Variant) (Pomodoro, error) {
+	ElapsedVal, ok := props["Elapsed"]
+	if !ok {
+		return Pomodoro{}, errors.New("No Elapsed value")
+	}
+	Elapsed, ok := ElapsedVal.Value().(float64)
+	if !ok {
+		return Pomodoro{}, errors.New("Elapsed value is of wrong type")
+	}
+
+	IsPausedVal, ok := props["IsPaused"]
+	if !ok {
+		return Pomodoro{}, errors.New("No IsPaused value")
+	}
+	IsPaused, ok := IsPausedVal.Value().(bool)
+	if !ok {
+		return Pomodoro{}, errors.New("IsPaused value is of wrong type")
+	}
+
+	StateVal, ok := props["State"]
+	if !ok {
+		return Pomodoro{}, errors.New("No State value")
+	}
+	State, ok := StateVal.Value().(string)
+	if !ok {
+		return Pomodoro{}, errors.New("State value is of wrong type")
+	}
+
+	StateDurationVal, ok := props["StateDuration"]
+	if !ok {
+		return Pomodoro{}, errors.New("No StateDuration value")
+	}
+	StateDuration, ok := StateDurationVal.Value().(float64)
+	if !ok {
+		return Pomodoro{}, errors.New("StateDuration value is of wrong type")
+	}
+
+	return Pomodoro{IsPaused, Elapsed, State, int(StateDuration)}, nil
 }
 
 func (m model) Init() tea.Cmd {
